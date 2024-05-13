148b9aeb72a8949e84cacf1e59db53d5e5e79a27
jcastillo21 <bcastillo-2022474@kinal.edu.gt>
2024-05-13 11:51:35 -0600

    WIP

--- src/pages/Post.tsx
+++ src/pages/Post.tsx
@@ -1,6 +1,7 @@
 import { Link, useLoaderData } from "react-router-dom";
 import Navbar from "../components/Navbar.tsx";
 import { useRef } from "react";
+import { IconArrowUp } from "@tabler/icons-react";
 
 // Inline Element Types
 interface TextElement {
@@ -143,6 +144,40 @@ export default function Post() {
             return <div>hola</div>;
           })}
         </article>
+      </div>
+      <div className="px-20 flex flex-col text-start">
+        <div>
+          <h3 className="font-bold pb-7">1 comment</h3>
+          <div className="py-2 px-5 border bg-white rounded flex flex-col gap-3">
+            <div className="flex gap-3 items-center w-full text-neutral-500">
+              <div className="rounded-full size-[35px] bg-red-600"></div>
+              <div>Username</div>
+              <div>Aug 15, 2023</div>
+            </div>
+            <span>Hola</span>
+            <div className="flex justify-between text-gray-600 text-sm">
+              <div className="flex gap-2 items-center">
+                <IconArrowUp size="14" className="cursor-pointer" />
+                <span>1</span>
+              </div>
+              <span>0 replies</span>
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col">
+          <div className="px-2 pt-2">
+            <span className="bg-white h-full px-3 py-2 border-x border-t border-black">
+              Write
+            </span>
+          </div>
+          <div className="p-2 bg-white">
+            <textarea
+              className="bg-neutral-300 w-full px-2 py-1"
+              placeholder="Write a comment..."
+              rows="5"
+            ></textarea>
+          </div>
+        </div>
       </div>
     </>
   );
